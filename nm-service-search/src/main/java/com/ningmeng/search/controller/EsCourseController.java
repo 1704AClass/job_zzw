25ad04477065f6f9acf84939fda21624cab87f96
zhaozhiwei <zhaozhiweies@163.com>
2020-03-02 11:02:54 +0800

    赵志伟在线教育项目

---
+++ nm-service-search/src/main/java/com/ningmeng/search/controller/EsCourseController.java
@@ -0,0 +1,24 @@
+package com.ningmeng.search.controller;
+
+import com.ningmeng.api.escourseapi.EsCourseControllerApi;
+import com.ningmeng.framework.domain.search.CourseSearchParam;
+import com.ningmeng.framework.model.response.QueryResponseResult;
+import com.ningmeng.search.service.EsCourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/search/course")
+public class EsCourseController implements EsCourseControllerApi {
+    @Autowired
+    EsCourseService esCourseService;
+
+    @Override
+    public QueryResponseResult list(@PathVariable("page") int page, @PathVariable("size") int size, CourseSearchParam courseSearchParam) throws IOException {
+        return esCourseService.list(page,size,courseSearchParam);
+    }
+}
